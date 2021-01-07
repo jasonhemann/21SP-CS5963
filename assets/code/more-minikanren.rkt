90f04e809bee764279a31f67726ee24ecef76e32
Jason Hemann <jasonhemann@gmail.com>
2021-01-07 11:43:37 -0500

    pushing up the rest of the work from today

--- assets/code/more-minikanren.rkt
+++ assets/code/more-minikanren.rkt
@@ -1,5 +1,7 @@
 #lang racket
 (require minikanren)
+(require racket/trace)
+(require rackunit)
 (require "numbers.rkt")
 
 (define (append ls1 ls2)
@@ -20,37 +22,81 @@
 
 ;; 1. implement reverse w/match & quasiquote and comma
 
-(define (reverse ls)
+(trace-define (reverse ls)
   (match ls
     [`() `()]
     [`(,a . ,d)
      (let ((res (reverse d)))
-       ;; guess what to do here.
-       )]))
+       (append res `(,a)))]))
+
+(trace-define (reverse2 ls acc)
+  (match ls
+    [`() acc]
+    [`(,a . ,d) (reverse2 d `(,a . ,acc))]))
 
 ;; (reverse '(a b c d)) => '(d c b a)
 ;;                                  how do I go from v to ^
 ;; (reverse   '(b c d)) => '(d c b)
 
-
-
-
 ;; reverseo list <-> list
 ;; the latter has the elements of the former in the opposite order
 
+(define (reverseo ls o)
+  (conde
+    [(== ls `()) (== o `())]
+    [(fresh (a d)
+       (== ls `(,a . ,d))
+       (fresh (res)
+         (reverseo d res)
+         (appendo res `(,a) o)))]))
+
 ;; 2. implement reverseo, the miniKanren relation.
 
-;; > (run 1 (q) (reverseo '(a b c d e) '(e d c b a)))
-;; '(_.0)
+(check-equal? (run 1 (q) (reverseo '(a b c d e) '(e d c b a))) '(_.0))
 
-;; > (run 1 (q) (reverseo '(a b c d e) '(e d c b)))
-;; '()
+(check-equal? (run 1 (q) (reverseo '(a b c d e) '(e d c b)))
+'())
 
-;; > (run* (q) (reverseo '(a b c d e) '(e d c b a)))
-;; '(_.0)
+(check-equal? (run* (q) (reverseo '(a b c d e) '(e d c b a)))
+ '(_.0))
 
-;; > (run 1 (q) (reverseo '(a b c d e) q))
-;; '((e d c b a))
+(check-equal? (run 1 (q) (reverseo '(a b c d e) q))
+'((e d c b a)))
 
-;; > (run 1 (q) (reverseo q '(a b c d e)))
-;; '((e d c b a))
+(check-equal? (run 1 (q) (reverseo q '(a b c d e)))
+ '((e d c b a)))
+
+;; "the remove relation"
+;; miniKanren numbers
+;; project
+
+(define (remove x ls)
+  (match ls
+    [`() `()]
+    [`(,a . ,d)
+     (cond
+       ((equal? x a) d)
+       ((not (equal? x a)) (let ((res (remove x d)))
+                             `(,a . ,res))))]))
+        
+(define (removeo x ls o)
+  (conde
+    ((== ls '()) (== o '()))
+    ((fresh (a d)
+       (== ls `(,a . ,d))
+       (conde
+         ((== x a) (== d o))
+         ((=/= x a)
+          (fresh (res)
+            (== `(,a . ,res) o)                    
+            (removeo x d res))))))))
+
+;; miniKanren numbers
+
+;; ones twos fours eights sixteens ...
+;; (1   1     0     0    1)
+;;  1 + 2 + 16
+
+;; start big -> go to the smaller bits
+;; start small go toward the larger bits***
+
